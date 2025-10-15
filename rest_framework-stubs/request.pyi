5aa03a0e44b70afa6bedba64f62b94cf6820922c
Konstantin Alekseev <konstantin@kotify.com>
2025-10-15 06:36:48 +0300

    fix request.user type

--- rest_framework-stubs/request.pyi
+++ rest_framework-stubs/request.pyi
@@ -5,7 +5,7 @@ from types import TracebackType
 from typing import Any
 
 from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import AnonymousUser, _User
+from django.contrib.auth.models import AnonymousUser
 from django.http import HttpRequest
 from django.http.request import QueryDict
 from rest_framework.authentication import BaseAuthentication
@@ -76,9 +76,9 @@ class Request(HttpRequest):
     def data(self) -> dict[str, Any] | list[Any]: ...
     @override  # type: ignore[explicit-override]
     @property
-    def user(self) -> _User | AnonymousUser: ...
+    def user(self) -> AbstractBaseUser | AnonymousUser: ...
     @user.setter
-    def user(self, value: _User | AnonymousUser) -> None: ...
+    def user(self, value: AbstractBaseUser | AnonymousUser) -> None: ...
     @property
     def auth(self) -> Token | Any: ...
     @auth.setter
